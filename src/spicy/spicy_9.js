4cc80de83cec4f57e643091e0d33d397b688aff1
Claire Liang <claire.liang.us@gmail.com>
2021-12-05 00:56:31 -0500

    hhhhhhhhh

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -47,7 +47,9 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-    
+    return function(num2) {
+        return num1 * num2;
+    }
 };
 
 
@@ -55,7 +57,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -63,7 +65,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
